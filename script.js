7e2b7f22985aaf00ed5784f5b9a52f086a2d19cd
nuthansai113 <130201140+nuthansai113@users.noreply.github.com>
2023-07-03 21:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  let radius = document.getElementById('radius').value;
+	let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	
+	document.getElementById('volume').value = volume.toFixed(4);
+}
+// Get the button element with id 'submit'
+let calculateButton = document.getElementById("submit");
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// Add an event listener for the click event on the button
+calculateButton.addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent form submission
+  volume_sphere(); // Call the volume_sphere function
+});
